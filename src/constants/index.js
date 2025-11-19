243bea5b1426f314538a788d02809cfd8b9a54e8
AnandPatil2710 <anandpatil72184@gmail.com>
2025-11-19 13:26:20 +0530

    commit

---
+++ src/constants/index.js
@@ -0,0 +1,311 @@
+const navLinks = [
+  {
+    name: "Work",
+    link: "#work",
+  },
+  {
+    name: "Experience",
+    link: "#experience",
+  },
+  {
+    name: "Skills",
+    link: "#skills",
+  },
+  {
+    name: "Testimonials",
+    link: "#testimonials",
+  },
+];
+
+const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+];
+
+const counterItems = [
+  { value: 1, suffix: "+", label: "Years of Experience" },
+  { value: 1, suffix: "+", label: "Satisfied Clients" },
+  { value: 3, suffix: "+", label: "Completed Projects" },
+  { value: 80, suffix: "%", label: "Client Retention Rate" },
+];
+
+const logoIconsList = [
+  {
+    imgPath: "/images/logos/company-logo-1.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-2.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-3.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-4.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-5.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-6.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-7.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-8.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-9.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-10.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-11.png",
+  },
+];
+
+const abilities = [
+  {
+    imgPath: "/images/seo.png",
+    title: "Quality Focus",
+    desc: "Delivering high-quality results while maintaining attention to every detail.",
+  },
+  {
+    imgPath: "/images/chat.png",
+    title: "Reliable Communication",
+    desc: "Keeping you updated at every step to ensure transparency and clarity.",
+  },
+  {
+    imgPath: "/images/time.png",
+    title: "On-Time Delivery",
+    desc: "Making sure projects are completed on schedule, with quality & attention to detail.",
+  },
+];
+
+const techStackImgs = [
+  {
+    name: "React Developer",
+    imgPath: "/images/logos/react.png",
+  },
+  {
+    name: "Python Developer",
+    imgPath: "/images/logos/python.svg",
+  },
+  {
+    name: "Backend Developer",
+    imgPath: "/images/logos/node.png",
+  },
+  {
+    name: "Interactive Developer",
+    imgPath: "/images/logos/three.png",
+  },
+  {
+    name: "Project Manager",
+    imgPath: "/images/logos/git.svg",
+  },
+];
+
+const techStackIcons = [
+  {
+    name: "React Developer",
+    modelPath: "/models/react_logo-transformed.glb",
+    scale: 1,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Tailwind CSS",
+    modelPath: "/models/3.glb",
+    scale: 1.8,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Java ",
+    modelPath: "/models/java.glb",
+    scale: 1.5,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Interactive Developer",
+    modelPath: "/models/three.js-transformed.glb",
+    scale: 0.05,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Project Manager",
+    modelPath: "/models/git-svg-transformed.glb",
+    scale: 0.05,
+    rotation: [0, -Math.PI / 4, 0],
+  },
+
+  //2nd
+  {
+    name: "SQL",
+    modelPath: "/models/sql.glb",
+    scale: 1.4,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Spring Boot",
+    modelPath: "/models/spring_boot.glb",
+    scale: 1,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Javascript",
+    modelPath: "/models/javascript_1.glb",
+    scale: 0.2,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "HTML",
+    modelPath: "/models/html.glb",
+    scale: 6,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "CSS",
+    modelPath: "/models/css.glb",
+    scale: 7,
+    rotation: [0, -Math.PI / 4, 0],
+  },
+];
+const expCards = [
+  {
+    review:
+      "Anand brought creativity and technical expertise to the team, significantly improving our frontend performance. His work has been invaluable in delivering faster experiences.",
+    imgPath: "/images/exp1.png",
+    logoPath: "/images/logo1.png",
+    title: "Frontend College Project (Zomato Clone)",
+    date: "January 2024 ",
+    responsibilities: [
+      "Developed and maintained user-facing features for the College Project (Zomato Clone) .",
+      "Collaborated closely with UI/UX designers to ensure seamless user experiences.",
+      "Optimized web applications for maximum speed and scalability.",
+    ],
+  },
+  {
+    review:
+      "Anand demonstrated exceptional technical and problem-solving skills during his internship at Seven Mentor Pvt Ltd. His contributions significantly improved the efficiency and functionality of our web systems.",
+    imgPath: "/images/exp2.png",
+    logoPath: "/images/logo2.png",
+    title: "Full Stack Developer- Intern",
+    date: "October 2024 - June 2024",
+    responsibilities: [
+      "Developed and maintained full-stack web applications using Java, Spring Boot, HTML, CSS, JavaScript, and MySQL.",
+      "Designed and optimized database schemas, queries, and relationships using MySQL",
+      "Worked with Servlets, JSP, and Thymeleaf for front-end rendering.",
+    ],
+  },
+  {
+    review:
+      "Anand consistently demonstrated a passion for  learning and development throughout his academic journey, contributing to various projects and honing his full-stack skills.",
+    imgPath: "/images/exp3.png",
+    logoPath: "/images/logo3.png",
+    title: "B.Tech in Information Technology",
+    date: "February 2020 - July 2024",
+    responsibilities: [
+      "Pursued B.Tech in IT at TGPCET, Nagpur.",
+      "Completed multiple academic and personal projects in full-stack development.",
+      "Actively participated in coding events, workshops, and internships to strengthen technical skills.",
+    ],
+  },
+];
+
+const expLogos = [
+  {
+    name: "logo1",
+    imgPath: "/images/logo1.png",
+  },
+  {
+    name: "logo2",
+    imgPath: "/images/logo2.png",
+  },
+  {
+    name: "logo3",
+    imgPath: "/images/logo3.png",
+  },
+];
+
+const testimonials = [
+  {
+    name: "Esther Howard",
+    mentions: "@estherhoward",
+    review:
+      "I can’t say enough good things about Anand. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
+    imgPath: "/images/client1.png",
+  },
+  {
+    name: "Wade Warren",
+    mentions: "@wadewarren",
+    review:
+      "Working with Anand was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+    imgPath: "/images/client3.png",
+  },
+  {
+    name: "Guy Hawkins",
+    mentions: "@guyhawkins",
+    review:
+      "Collaborating with Anand was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Anand enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Anand is the ideal partner.",
+    imgPath: "/images/client2.png",
+  },
+  {
+    name: "Marvin McKinney",
+    mentions: "@marvinmckinney",
+    review:
+      "Anand was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
+    imgPath: "/images/client5.png",
+  },
+  {
+    name: "Floyd Miles",
+    mentions: "@floydmiles",
+    review:
+      "Anand expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
+    imgPath: "/images/client4.png",
+  },
+  {
+    name: "Albert Flores",
+    mentions: "@albertflores",
+    review:
+      "Anand was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
+    imgPath: "/images/client6.png",
+  },
+];
+
+const socialImgs = [
+  {
+    name: "insta",
+    lurl: "https://www.instagram.com/_.im_pixl/?hl=en",
+    imgPath: "/images/insta.png",
+  },
+  {
+    name: "x",
+    lurl: "https://x.com/AnandPatil2710",
+    imgPath: "/images/x.png",
+  },
+  {
+    name: "linkedin",
+    lurl: "https://www.linkedin.com/in/anandpatil2210/",
+    imgPath: "/images/linkedin.png",
+  },
+];
+
+export {
+  words,
+  abilities,
+  logoIconsList,
+  counterItems,
+  expCards,
+  expLogos,
+  testimonials,
+  socialImgs,
+  techStackIcons,
+  techStackImgs,
+  navLinks,
+};
